9c57e755f30806c5a79e1ffd6b00674063ddb056
Atnanasi <ys2000pro@gmail.com>
2018-08-19 19:03:37 +0900

    Use official certbot image

--- getcert.sh
+++ getcert.sh
@@ -6,13 +6,12 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-IMAGE_NAME_WITH_VERSION=$IMAGE_NAME:v$CERTBOT_VERSION
+IMAGE_NAME_WITH_VERSION=$IMAGE_NAME:$CERTBOT_VERSION
 
 # Is docker image exist?
 docker inspect --type=image $IMAGE_NAME_WITH_VERSION > /dev/null 2>&1
 if [ 0 -ne $? ]; then
-    docker build -t $IMAGE_NAME --build-arg CERTBOT_VERSION=$CERTBOT_VERSION .
-    docker tag $IMAGE_NAME $IMAGE_NAME_WITH_VERSION
+	docker pull $IMAGE_NAME_WITH_VERSION
 fi
 
 if [ -e $(pwd)/config/live/$1 ]; then
